22072c00e6550684b27aee0f05ea96a9b599b23e
Rom Vireak <vireakrom69@gmail.com>
2025-05-13 22:55:48 +0700

    Update

--- FreeBook/index.php
+++ FreeBook/index.php
@@ -5,8 +5,6 @@
     <title>Dashboard - FreeBooks</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <link rel="icon" type="image/png" href="images/book-icon.png" sizes="32x32">
-
     <!-- Rest of head content -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha256-eZrrJcwDc/3uDhsdt61sL2oOBY362qM3lon1gyExkL0=" crossorigin="anonymous">
